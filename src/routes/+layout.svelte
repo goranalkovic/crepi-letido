b82873cc37b43c298a5f1df294bee6b320dd86a1
Goran Alković <goranalkovic@users.noreply.github.com>
2023-08-03 00:21:53 +0200

    add transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,23 @@
 <script>
 	import '../app.postcss';
+
+	import { fly } from 'svelte/transition';
+	import { circIn, circOut } from 'svelte/easing';
+
+	export let data;
+
+    const direction = data.url === '/gableci/results' ? 1 : -1;
 </script>
 
 <div class="app">
 	<main>
-		<slot />
+		{#key data.url}
+			<div
+				in:fly={{ x: direction * -100, duration: 400, delay: 350, easing: circOut }}
+				out:fly={{ x: direction * 100, duration: 300, easing: circIn }}
+			>
+				<slot />
+			</div>
+		{/key}
 	</main>
 </div>
