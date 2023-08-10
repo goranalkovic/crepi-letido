fceb4553c794249418468c0337ad7bae0bfb40b2
Goran Alković <goranalkovic@users.noreply.github.com>
2023-08-10 08:37:08 +0000

    add view shortcuts guide

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script>
 	import { Canvas } from '@threlte/core';
+	import { Kbd } from 'flowbite-svelte';
 	import CrepModel from '$lib/CrepModel.svelte';
+	import Mouse from 'phosphor-svelte/lib/Mouse';
+	import MouseSimple from 'phosphor-svelte/lib/MouseSimple';
 </script>
 
 <svelte:head>
@@ -12,4 +15,32 @@
 	<Canvas>
 		<CrepModel />
 	</Canvas>
+</div>
+
+<div
+	class="fixed bottom-5 left-5 z-10 pointer-events-none select-none flex flex-wrap items-center gap-8"
+>
+	<div class="flex items-center gap-2">
+		<Kbd class="px-2 py-1.5"><Mouse size="16" color="currentColor" /></Kbd>
+		<span>Rotate/orbit</span>
+	</div>
+
+	<div class="flex items-center gap-2">
+		<span>
+			<Kbd class="px-2 py-1.5">Ctrl</Kbd>
+			+
+			<Kbd class="px-2 py-1.5"><Mouse size="16" color="currentColor" /></Kbd>
+		</span>
+		<span>
+			<Kbd class="px-2 py-1.5">Shift</Kbd>
+			+
+			<Kbd class="px-2 py-1.5"><Mouse size="16" color="currentColor" /></Kbd>
+		</span>
+		<span>Pan</span>
+	</div>
+
+	<div class="flex items-center gap-2">
+		<Kbd class="px-2 py-1.5"><MouseSimple size="16" color="currentColor" /></Kbd>
+		<span>Zoom</span>
+	</div>
 </div>
