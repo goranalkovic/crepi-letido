680f539f7f770970159e3bde9bff89b7a676bdd6
Goran Alković <goranalkovic@users.noreply.github.com>
2023-08-18 06:09:33 +0200

    fix url

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -36,7 +36,7 @@
 		const { data, error } = await supabase.auth.signInWithOtp({
 			email: email,
 			options: {
-				emailRedirectTo: `${location.origin}/auth/callback`
+				emailRedirectTo: `${location.origin}/profile/callback`
 			}
 		});
 
